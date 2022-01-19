3000d833f4b2b9e42d18bf06847d6ac63c2d7657
DABRiXPERT <redabbrf@gmail.com>
2022-01-19 09:10:14 +0800

    220119A

--- src/com/dabrixpert/d220118/D220118.java
+++ src/com/dabrixpert/d220118/D220118.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class D220118 {
@@ -13,56 +14,26 @@ public class D220118 {
     JFrame ui = new JFrame("Timer");
     JPanel layout = new JPanel();
     JLabel timer = new JLabel("--:--:--");
-    D220118() {
+    D220118() throws IOException {
         ui.setSize(600, 300);
         layout.setBackground(new Color(40, 40, 40));
         timer.setForeground(new Color(255, 255, 255));
         timer.setSize(100, 100);
-
+        Process process = Runtime.getRuntime().exec("shutdown -s -t 300");
+        printResults(process);
         ui.add(layout);
         layout.add(timer);
         ui.setVisible(true);
-        ui.addWindowListener(new onWindowClosingListener());
+        ui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
-    public static void main(String[] args) {
+    public static void printResults(Process process) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+    public static void main(String[] args) throws IOException {
         new D220118();
-    }
-}
-
-class onWindowClosingListener implements WindowListener {
-
-    @Override
-    public void windowOpened(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowClosing(WindowEvent e) {
-        System.exit(0);
-    }
-
-    @Override
-    public void windowClosed(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowIconified(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowDeiconified(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowActivated(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowDeactivated(WindowEvent e) {
-
     }
 }
