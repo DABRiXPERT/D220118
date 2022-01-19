5a0fb00570c8c96cac98755829fd8e87d8ce252a
DABRiXPERT <redabbrf@gmail.com>
2022-01-19 09:37:02 +0800

    220119B

--- src/com/dabrixpert/d220118/D220118.java
+++ src/com/dabrixpert/d220118/D220118.java
@@ -2,11 +2,14 @@ package com.dabrixpert.d220118;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Date;
 
 public class D220118 {
 
@@ -17,10 +20,25 @@ public class D220118 {
     D220118() throws IOException {
         ui.setSize(600, 300);
         layout.setBackground(new Color(40, 40, 40));
+        layout.setLayout(null);
         timer.setForeground(new Color(255, 255, 255));
-        timer.setSize(100, 100);
-        Process process = Runtime.getRuntime().exec("shutdown -s -t 300");
-        printResults(process);
+        timer.setBounds(0, 0, 600, 80);
+        timer.setFont(new Font("MS Gothic", Font.PLAIN, 40));
+//        Process process = Runtime.getRuntime().exec("shutdown -s -t 300");
+//        printResults(process);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Timer t = new Timer(0, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        timer.setText(String.valueOf(System.currentTimeMillis()));
+                    }
+                });
+                t.start();
+            }
+        });
+        thread.run();
         ui.add(layout);
         layout.add(timer);
         ui.setVisible(true);
